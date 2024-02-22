4836ec9194eaac2e0048973ffa527adb1886c469
Half_nothing <Half_nothing@163.com>
2024-02-22 21:33:20 +0800

    remove MCBBS BMCLAPIDownloadProvider

--- HMCL/src/main/java/org/jackhuang/hmcl/setting/DownloadProviders.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/setting/DownloadProviders.java
@@ -53,7 +53,7 @@ public final class DownloadProviders {
 
     private static final MojangDownloadProvider MOJANG;
     private static final BMCLAPIDownloadProvider BMCLAPI;
-    private static final BMCLAPIDownloadProvider MCBBS;
+    //    private static final BMCLAPIDownloadProvider MCBBS;
     private static final InvalidationListener observer;
     private static DownloadProvider currentDownloadProvider;
 
@@ -64,21 +64,21 @@ public final class DownloadProviders {
 
         MOJANG = new MojangDownloadProvider();
         BMCLAPI = new BMCLAPIDownloadProvider(bmclapiRoot);
-        MCBBS = new BMCLAPIDownloadProvider("https://download.mcbbs.net");
+//        MCBBS = new BMCLAPIDownloadProvider("https://download.mcbbs.net");
         rawProviders = mapOf(
                 pair("mojang", MOJANG),
-                pair("bmclapi", BMCLAPI),
-                pair("mcbbs", MCBBS)
+                pair("bmclapi", BMCLAPI)
+//                pair("mcbbs", MCBBS)
         );
 
         AdaptedDownloadProvider fileProvider = new AdaptedDownloadProvider();
-        fileProvider.setDownloadProviderCandidates(Arrays.asList(MCBBS, BMCLAPI, MOJANG));
-        BalancedDownloadProvider balanced = new BalancedDownloadProvider(Arrays.asList(MCBBS, BMCLAPI, MOJANG));
+        fileProvider.setDownloadProviderCandidates(Arrays.asList(BMCLAPI, MOJANG));
+        BalancedDownloadProvider balanced = new BalancedDownloadProvider(Arrays.asList(BMCLAPI, MOJANG));
 
         providersById = mapOf(
                 pair("official", new AutoDownloadProvider(MOJANG, fileProvider)),
-                pair("balanced", new AutoDownloadProvider(balanced, fileProvider)),
-                pair("mirror", new AutoDownloadProvider(MCBBS, fileProvider)));
+                pair("balanced", new AutoDownloadProvider(balanced, fileProvider)));
+//                pair("mirror", new AutoDownloadProvider(MCBBS, fileProvider)));
 
         observer = FXUtils.observeWeak(() -> {
             FetchTask.setDownloadExecutorConcurrency(
